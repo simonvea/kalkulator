4d9feaf2a5d44ba7cf75e259c28565b948e67b8e
Simon Opheim <simon.opheim@gmail.com>
2019-05-28 12:34:28 +0200

    refactored code

--- skript.js
+++ skript.js
@@ -1,31 +1,90 @@
 
-
+const display = document.getElementById("display");
 const numbers = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators");
-const display = document.getElementById("display")
+const backspaceButton = document.getElementById("backspace");
+const clearButton = document.getElementById("clear");
+const sumButton = document.getElementById("sum");
 
 let screenValue = 0;
-let presentValue;
 let oldValue;
-let sum = 0;
-let action;
+let action = false;
 
-display.textContent = screenValue;
+numbers.forEach((number) => {
+    number.addEventListener('click', (e) => {
+        e.target.classList.add('press');
+        setScreenValue(number.id);
+    });
+}); 
 
-//faktisk funksjonalitet
-function setScreenValue(inputNumber) {  // prev useNumber
+function setScreenValue(inputNumber) {
+    const maxDisplayLength = 6;
+ 
+    if (screenValue == 0) {screenValue = inputNumber}
+    else if (screenValue % 1 != 0) {parseFloat(screenValue, 10).toFixed(1)} //hvis det er decimaler i input får de bare lov å bruke ett.
+    else if(screenValue.length >= maxDisplayLength) {screenValue}
+    else {screenValue += inputNumber};
 
-    if(inputNumber == "backspace") {
-        removeLastNumber();
-    } else {
-        //viser tallene på displayet og lagrer verdiene i screenValue.
-        if (screenValue == 0) {screenValue = inputNumber}
-        else if (screenValue % 1 != 0) {parseFloat(screenValue,10).toFixed(1)} //hvis det er decimaler i input får de bare lov å bruke ett.
-        else if(screenValue.length >= 6) {screenValue} //forhindrer display overflow
-        else {screenValue += inputNumber};
-        display.textContent = screenValue;
-    };  
+    display.textContent = screenValue;
 }
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', (e) => {
+        e.target.classList.add('press');
+        saveAction(operator.id);
+    });
+}); 
+
+function saveAction(operation) {
+    const presentValue = parseFloat(screenValue,10);
+
+    if(action) {
+        oldValue = operator(action, oldValue, presentValue);
+        action = operation;
+    } else {
+        oldValue = presentValue;
+        action = operation;
+    }
+    
+    screenValue = 0;
+};
+
+function operator(operation, a, b) {
+    switch(operation) {
+        case "add":
+            return a+b;
+        case "sub":
+            return a-b;
+        case "multiply":
+            return a*b;
+        case "divide":
+            return a/b
+    }
+}
+
+sumButton.addEventListener("click", (e) => {
+    e.target.classList.add('press');
+    calculateSum();
+});
+
+function calculateSum() { 
+    if (action == "divide" && screenValue == 0) { // om luringen prøver å dele på 0..
+        clearVariables();
+        display.textContent = "TULLING";
+    } else { //summer, og vis
+        const newValue = parseFloat(screenValue, 10);
+        let sum = operator(action, oldValue, newValue);
+        if(sum.toString().length > 6) {sum = sum.toExponential(3)};
+        screenValue = sum;
+        display.textContent = screenValue;
+        action = false;
+    };
+}
+
+backspaceButton.addEventListener("click", (e) => {
+    e.target.classList.add('press');
+    removeLastNumber();
+});
 
 function removeLastNumber() {
     if(screenValue.toString().length <= 1) {screenValue = 0}
@@ -33,96 +92,30 @@ function removeLastNumber() {
     display.textContent = screenValue;
 }
 
-function actions(id) {
-
-    if (id == "sum") {
-        //det som ble trykket på før =
-        presentValue = parseFloat(screenValue,10);
-
-        //sjekker om luringen prøver å dele på 0
-
-        if (action == "divide" && screenValue == 0) {
-
-            clearVariables();
-            
-            display.textContent = "TULLING";
-
-        } else { //summer, og vis
-
-            sum = operator(window[action], oldValue, presentValue);
-            if(sum.toString().length > 6) {sum = sum.toExponential(3)};
-            oldValue = sum;
-            display.textContent = sum;
-        };
-        
-
-    } else if (id == "clear") {
-        clearVariables()
-    } else { //hvis det blir klikket på noen av operatorene..
-        
-        presentValue = parseFloat(screenValue,10); //lagrer input
-
-        //hvis det allerede finnes en gammel verdi, summer verdiene basert på forrige operator
-        if(oldValue !== undefined) {sum = operator(window[action], oldValue, presentValue)}
-
-        //arkiverer input eller gammel verdi
-        oldValue !== undefined ? oldValue = sum : oldValue = presentValue;  
-
-        action = id;  //lagrer hva som ble trykket på
-    };
-
-    screenValue = 0; //nullstiller display
-};
-
-//math functions below
-function add(a,b) {
-    return a + b;
-};
-
-function sub(a,b) {
-    return a - b;
-};
-
-function multiply(a,b) {
-    return a*b;
-};
-
-function divide(a,b) {
-    return a/b;
-};
-
-function operator(operation, a, b) {
-    return operation(a,b);
-}
-
+clearButton.addEventListener("click", (e) => {
+    e.target.classList.add('press');
+    clearVariables();
+});
 
 function clearVariables() {
     screenValue = 0;
-    presentValue = undefined;
-    oldValue = undefined;
-    sum = 0;
+    oldValue = 0;
+    action = false;
     display.textContent = screenValue;
 };
 
-//do stuff
-//MUSEKLIKK
-//legger til klikkmuligheter på tall
-numbers.forEach((number) => {
-    number.addEventListener('click', (e) => {
-        const nmbr = document.querySelector(`div[id="${number.id}"]`)
-        useNumber(number.id)
-        nmbr.classList.add('press');
-    });
-}); 
 
-//legger til klikkmuligheter på operatorene
-operators.forEach((op) => {
-    op.addEventListener('click', (e) => {
-        const nmbr = document.querySelector(`div[id="${op.id}"]`)
-        actions(op.id)
-        nmbr.classList.add('press');
-    });
-}); 
+
+
+
+
+
+
+/* 
+===========================
+==== EventListeners =======
+===========================
+ */
 
 //TASTATUR
 // sjekker om symboler blir trykket på og utførerer evt. handlinger. (tastatur)
@@ -131,7 +124,7 @@ window.addEventListener("keypress", (e) => {
     if(!key) {
         const enterKey = document.querySelector(`div[data-char2="${e.charCode}"]`);
         if (!enterKey) return;
-        actions(enterKey.id);
+        calculateSum();
         return;
     };
 
@@ -142,21 +135,26 @@ window.addEventListener("keypress", (e) => {
 
 //sjekker om tall blir trykket og utfører evt. handlinger (tastatur)
 window.addEventListener("keydown", (e) => {
-    if(e.keyCode == 8) {e.preventDefault()};
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if(e.shiftKey) return; //i tilfelle det blir trykket "=", som innebærer shift, så ikke gjør noe..
-    if(!key) return;
-    useNumber(key.id);
-
+    if(e.keyCode == 8) {
+        e.preventDefault();
+        removeLastNumber();
+    } else if (e.shiftKey) { //i tilfelle det blir trykket "=", som innebærer shift, så ikke gjør noe..
+        return;
+    } else if (!key) {
+        return;
+    } else {
+        setScreenValue(key.id);
+    }
     key.classList.add('press');
 });
 
 //transition effects
+
+const buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener('transitionend', removeTransition));
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('press');
   }
-
-
-const knapper = Array.from(document.querySelectorAll('.numbers, .operators'));
-knapper.forEach(key => key.addEventListener('transitionend', removeTransition));
